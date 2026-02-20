c8e1e109040b1188b86929f5adef118be6e40c6e
Keyao-Trinh <trinhkeyao@gmail.com>
2026-02-19 19:56:02 -0500

    I think I almost have things fixed

--- vite/src/main.js
+++ vite/src/main.js
@@ -1,17 +1,9 @@
 // import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+// import { alert } from './index.html'
 
 // document.querySelector('#app').innerHTML = `
 //   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
+//     <h1>Hi</h1>
 //     <div class="card">
 //       <button id="counter" type="button"></button>
 //     </div>
@@ -21,4 +13,4 @@
 //   </div>
 // `
 
-// setupCounter(document.querySelector('#counter'))
+// 
